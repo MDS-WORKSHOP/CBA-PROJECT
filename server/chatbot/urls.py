6f2f929b0a5f5a987d99bbdef0493098300c20e5
edoisy <edoisy@relatia.fr>
2024-05-14 20:36:43 +0200

    feat(URLs): Add URL routes for API endpoints and JWT authentication
    - Add URL routes for User, Conversation, Message, Request, Instrument, Equivalent, InstrumentFeature, and File API endpoints
    - Add URL routes for JWT token obtain and refresh endpoints

--- server/chatbot/urls.py
+++ server/chatbot/urls.py
@@ -1,6 +1,17 @@
 from django.urls import path
 from .views import TestConnectionAPI
+from .views import UserViewSet, ConversationViewSet, MessageViewSet, RequestViewSet, InstrumentViewSet, EquivalentViewSet, InstrumentFeatureViewSet, FileViewSet
+from rest_framework.routers import DefaultRouter
 
+router = DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'conversations', ConversationViewSet)
+router.register(r'messages', MessageViewSet)
+router.register(r'requests', RequestViewSet)
+router.register(r'instruments', InstrumentViewSet)
+router.register(r'equivalents', EquivalentViewSet)
+router.register(r'instrument_features', InstrumentFeatureViewSet)
+router.register(r'files', FileViewSet)
 
 
 urlpatterns = [
