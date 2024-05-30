b466e6f2aeea140cbf8ad30adcaf2f5e4b5cdd06
Lucas Malatchoumy <malatchoumylucas.com>
2024-05-30 11:36:42 +0200

    feat(Component): create reusable components

---
+++ client/src/components/Button.vue
@@ -0,0 +1,34 @@
+<!-- src/components/Button.vue -->
+<template>
+  <button class="text-white w-full" :class="classes" @click="handleClick" :disabled="disabled">
+    <slot />
+  </button>
+</template>
+
+<script setup lang="ts">
+import { defineProps, computed } from 'vue'
+
+const props = defineProps({
+  type: { type: String, default: 'button' },
+  color: { type: String, default: 'bg-blue-500' },
+  size: { type: String, default: 'p-2' },
+  disabled: { type: Boolean, default: false },
+  onClick: Function
+})
+
+const classes = computed(() => [
+  props.color,
+  props.size,
+  props.disabled ? 'opacity-50 cursor-not-allowed' : ''
+].join(' '))
+
+const handleClick = (event: Event) => {
+  if (!props.disabled && props.onClick) {
+    props.onClick(event)
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ client/src/components/Input.vue
@@ -0,0 +1,24 @@
+
+<template>
+  <div class="relative w-full py-2">
+    <input
+      v-model="modelValue"
+      :placeholder="props.placeholder"
+      :type="props.type"
+      class="p-2 pl-8 border border-gray-300 w-full"
+      v-bind="$attrs"
+    />
+    <slot name="icon" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+const modelValue = defineModel()
+const props = defineProps({
+  placeholder: { type: String, default: 'Enter your text' },
+  type: { type: String, default: 'text' },
+})
+
+</script>
