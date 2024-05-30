d2933f6b9822362c9c879d4ea11e5a8678ee86ab
Lucas Malatchoumy <malatchoumylucas.com>
2024-05-30 11:33:23 +0200

    feat(Structure): create routers and init store

--- client/src/router.ts
+++ client/src/router.ts
@@ -1,28 +1,57 @@
 import { createWebHistory, createRouter } from 'vue-router'
-
-import Home from './Home.vue'
+import DefaultLayout from './layouts/DefaultLayout.vue'
+import AuthLayout from './layouts/Auth.vue'
+import Login from './Login.vue'
 import RequestResetPassword from './RequestResetPassword.vue'
 import ResetPassword from './ResetPassword.vue'
-import Home1 from './Home1.vue'
+import Home from './Home.vue'
 
 const routes = [
-  { path: '/', component: Home1 },
-  { path: '/login', component: Home },
-  { path: '/request-reset-password', component: RequestResetPassword },
-  { path: '/reset-password', component: ResetPassword },
-]
+  {
+    path: '/',
+    component: DefaultLayout,
+    children: [
+      { path: '', component: Home }
+    ]
+  },
+  {
+    path: '/login',
+    component: AuthLayout,
+    children: [
+      { path: '', component: Login }
+    ]
+  },
+  {
+    path: '/request-reset-password',
+    component: AuthLayout,
+    children: [
+      { path: '', component: RequestResetPassword }
+    ]
+  },
+  {
+    path: '/reset-password',
+    component: AuthLayout,
+    children: [
+      { path: '', component: ResetPassword }
+    ]
+  }
+];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
 })
+
 router.beforeEach((to, from, next) => {
-  const publicPages = ['/login', '/request-reset-password', '/reset-password']
+  const publicPages = ['/', '/login', '/request-reset-password', '/reset-password']
   const authRequired = !publicPages.includes(to.path)
   const loggedIn = localStorage.getItem('accessToken')
+
   if (authRequired && !loggedIn) {
     return next('/login')
   }
+
   next()
 })
+
 export default router
