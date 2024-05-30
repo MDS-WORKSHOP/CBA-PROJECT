24b38fe80ed6bd57caea7e4b836dee3d7553a0bc
Lucas Malatchoumy <malatchoumylucas.com>
2024-05-30 11:01:10 +0200

    feat(Style): set custom font and colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        title: '#091B62',
+        subtitle: '#666975',
+        button: '#0800B9'
+      },
+      fontFamily: {
+        norms: ['"TT Norms Pro"', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
