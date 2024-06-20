8f0c6857f1b338764f58f4391df49d381f283daa
Lucas Malatchoumy <malatchoumylucas.com>
2024-06-20 22:47:01 +0200

    feat(Icons): update icons components

---
+++ client/src/components/Icons/ExitIcon.vue
@@ -0,0 +1,16 @@
+<template>
+<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M9.88281 19.125V17.0417H17.1745V2.45833H9.88281V0.375H19.2578V19.125H9.88281ZM7.79948 14.9583L6.36719 13.4479L9.02344 10.7917H0.507812V8.70833H9.02344L6.36719 6.05208L7.79948 4.54167L13.0078 9.75L7.79948 14.9583Z" fill="#051039"/>
+</svg>
+
+
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  size: { type: Number, default: 24 },
+})
+
+</script>
---
+++ client/src/components/Icons/Reject.vue
@@ -0,0 +1,15 @@
+<template>
+<svg width="25" height="26" viewBox="0 0 25 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M2.86531 25.0415L0.457031 22.6332L10.0901 13.0001L0.457031 3.36701L2.86531 0.95874L12.4984 10.5918L22.1315 0.95874L24.5398 3.36701L14.9067 13.0001L24.5398 22.6332L22.1315 25.0415L12.4984 15.4084L2.86531 25.0415Z" fill="#DF0404"/>
+</svg>
+
+  </template>
+  
+  <script setup lang="ts">
+  import { defineProps } from 'vue'
+  
+  const props = defineProps({
+    size: { type: Number, default: 24 },
+  })
+  
+  </script>
