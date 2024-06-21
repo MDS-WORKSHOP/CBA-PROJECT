a76baafd3db729c423b3cf3d9a5285da449f23d9
Lucas Malatchoumy <malatchoumylucas.com>
2024-06-21 10:22:44 +0200

    chore(Package): use notification library

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import './style.css'
 import router from './router'
 import { createPinia } from 'pinia';
+import ToastPlugin from 'vue-toast-notification';
+import 'vue-toast-notification/dist/theme-default.css';
 import App from './App.vue'
 
 const app = createApp(App);
@@ -9,4 +11,5 @@ const pinia = createPinia();
 
 app.use(router);
 app.use(pinia);
+app.use(ToastPlugin);
 app.mount('#app');
